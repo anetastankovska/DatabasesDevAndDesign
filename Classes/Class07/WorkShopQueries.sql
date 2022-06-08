cd854101a18bc5bafdb772edf2e263ac4391ccca
anetastankovska <anetastankovskaane@gmail.com>
2022-06-08 21:08:03 +0200

    Class 7

---
+++ Classes/Class07/WorkShopQueries.sql
@@ -0,0 +1,71 @@
+CREATE OR ALTER FUNCTION GetUserFullName(
+	@FirstName nvarchar(100),
+	@LastName nvarchar(100)
+)
+RETURNS NVARCHAR(255)
+AS
+BEGIN
+	RETURN CONCAT(@FirstName ,' ', @LastName );
+END
+GO
+
+SELECT dbo.GetUserFullName(u.FirstName, u.LastName)
+FROM Users u
+GO
+
+select * from Pizzas
+select * from OrderDetails
+select * from Orders
+select * from Users
+GO
+
+--My code
+CREATE VIEW [Users waiting for orders]
+AS
+SELECT p.Name, u.FirstName, u.LastName, o.IsDelivered
+FROM Users u
+INNER JOIN Orders o ON u.Id = o.UserId
+INNER JOIN OrderDetails od ON od.OrderId = o.Id
+INNER JOIN Pizzas p on p.Id = od.PizzaId
+WHERE o.IsDelivered = 0
+GROUP BY p.Name, u.FirstName, u.LastName, o.IsDelivered
+GO
+
+--Martin's code
+SELECT p.Name, u.FirstName, u.LastName
+FROM Pizzas p
+INNER JOIN OrderDetails od ON od. PizzaId = p.Id
+INNER JOIN Orders o ON o.Id = od.OrderId
+INNER JOIN Users u ON u.Id = o.UserId
+WHERE o.IsDelivered =0
+GO
+
+
+SELECT Name, FirstName, LastName, COUNT(*) AS NumberOfOrders from [Users waiting for orders]
+GROUP BY Name, FirstName, LastName
+GO
+
+--
+CREATE OR ALTER PROCEDURE [Insert pizza in DB]
+AS
+
+BEGIN
+	INSERT INTO Pizzas(Name, SizeId, Price)
+	VALUES('Vegetariana', 2, 350)
+END
+SELECT TOP 1 Id FROM Pizzas
+ORDER BY Id DESC 
+
+EXECUTE [Insert pizza in DB]
+
+
+
+
+
+
+
+
+
+
+
+
